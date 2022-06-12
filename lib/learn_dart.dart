1c9733b6002a93597aada889dee7988f3f6e577a
Luca Virgilio <luca.virgilio95@gmail.com>
2022-06-12 22:59:33 +0200

    Add different widgets

--- lib/learn_dart.dart
+++ lib/learn_dart.dart
@@ -8,15 +8,88 @@ class LearnFlutterPage extends StatefulWidget {
 }
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwitch = false;
+  bool? isCheckbox = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Learn Flutter'),
         automaticallyImplyLeading: false,
-        leading: IconButton(onPressed:(){
-          Navigator.of(context).pop();
-        }, icon: const Icon(Icons.arrow_back_ios)),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(Icons.arrow_back_ios)),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('images/einstein.jpg'),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  'This is a title',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              color: Colors.blue,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  primary: isSwitch ? Colors.green : Colors.blue),
+              onPressed: () {
+                debugPrint('click elevated button');
+              },
+              child: const Text('elevated button'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                debugPrint('click outlined button');
+              },
+              child: const Text('outlined button'),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint('click text button');
+              },
+              child: const Text('text button'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.access_time_filled_rounded),
+                Text('Row widget'),
+                Icon(Icons.access_time_filled_rounded)
+              ],
+            ),
+            Switch(
+                value: isSwitch,
+                onChanged: (bool newBool) {
+                  setState(() {
+                    isSwitch = newBool;
+                  });
+                }),
+            Checkbox(
+                value: isCheckbox,
+                onChanged: (bool? newBool) {
+                  setState(() {
+                    isCheckbox = newBool;
+                  });
+                }),
+            Image.network(
+                'https://thumbs.dreamstime.com/z/pop-art-di-albert-einstein-51720486.jpg'),
+          ],
+        ),
       ),
     );
   }
