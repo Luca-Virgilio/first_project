9e9b35867c6a9421c60af32f1e84ee51d70ed98f
Luca Virgilio <luca.virgilio95@gmail.com>
2022-06-12 17:57:39 +0200

    Create navigation structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_project/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'First Project',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,11 +26,46 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: Scaffold()
+      home: RootPage(),
     );
   }
 }
 
+class RootPage extends StatefulWidget {
+  const RootPage({Key? key}) : super(key: key);
 
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
 
+class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("First Project"),
+      ),
+      body: HomePage(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint("click button");
+        },
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.person), label: "Profile")
+        ],
+        onDestinationSelected: (int index){
+          setState((){
+            currentPage = index;
+          });
+        },
+        selectedIndex: currentPage,
+      ),
+    );
+  }
+}
